1f54f8882085fa1f1faf8a0ccc87c514e65fc868
unknown <renan.rodrigues@mitahtech.com>
2016-01-09 21:00:31 -0200

    develop: definindo a view header do projeto

---
+++ frontend/app/scripts/controllers/headerController.js
@@ -0,0 +1,49 @@
+(function() {
+
+	var modulo = angular.module('mercadaoModule');
+
+	modulo.controller('headerController', function($scope, $rootScope, $location) {
+		$scope.itemLocality = "";
+
+		$scope.init = function(){
+			$scope.geocoder = new google.maps.Geocoder();
+
+			var codeLatLng = function (lat, lng) {
+			  var latlng = new google.maps.LatLng(lat, lng);
+			  $scope.geocoder.geocode({latLng: latlng}, function(results, status) {
+			    if (status == google.maps.GeocoderStatus.OK) {
+				    if (results[1]) {
+				      	var arrAddress = results;
+				      	$.each(arrAddress, function(i, address_component) {
+				         	if (address_component.types[0] == "locality") {
+				            	$scope.itemLocality = address_component.address_components[0].long_name;
+				            	$scope.$apply();
+				          	}
+				        });
+			      	} else {
+			     	   console.log("No results found");
+		    		}
+			    } else {
+			      console.log("Geocoder failed due to: " + status);
+			    }
+			  });
+			};
+			
+			// Get the latitude and the longitude;
+			var successFunction = function (position) {
+			  var lat = position.coords.latitude;
+			  var lng = position.coords.longitude;
+			  codeLatLng(lat, lng);
+			};
+
+			var errorFunction =  function () {
+			  alert("Geocoder failed");
+			};
+			
+			if (navigator.geolocation) {
+			  navigator.geolocation.getCurrentPosition(successFunction, errorFunction);
+			}
+		};
+	});
+
+})();
--- frontend/app/scripts/controllers/inicialController.js
+++ frontend/app/scripts/controllers/inicialController.js
@@ -3,8 +3,7 @@
 	var modulo = angular.module('mercadaoModule');
 
 	modulo.controller('InicialController', function($scope, $rootScope, $location) {
-		console.log('teste de console');
-		
+		//console.log('teste de console');
 	});
 
 })();
