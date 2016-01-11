d249fd4c2ee571ede45a7b39fd0196663283f3d2
Julio Cezar <julio@casa>
2016-01-11 19:24:43 -0200

    Correção da controller do Header para ficar nos padrões do JSHint

--- frontend/app/scripts/controllers/headerController.js
+++ frontend/app/scripts/controllers/headerController.js
@@ -5,45 +5,69 @@
 	modulo.controller('headerController', function($scope, $rootScope, $location) {
 		$scope.itemLocality = "";
 
-		$scope.init = function(){
+		$scope.init = function() {
 			$scope.geocoder = new google.maps.Geocoder();
 
-			var codeLatLng = function (lat, lng) {
-			  var latlng = new google.maps.LatLng(lat, lng);
-			  $scope.geocoder.geocode({latLng: latlng}, function(results, status) {
-			    if (status == google.maps.GeocoderStatus.OK) {
-				    if (results[1]) {
-				      	var arrAddress = results;
-				      	$.each(arrAddress, function(i, address_component) {
-				         	if (address_component.types[0] == "locality") {
-				            	$scope.itemLocality = address_component.address_components[0].long_name;
-				            	$scope.$apply();
-				          	}
-				        });
-			      	} else {
-			     	   console.log("No results found");
-		    		}
-			    } else {
-			      console.log("Geocoder failed due to: " + status);
-			    }
-			  });
+			var codeLatLng = function(lat, lng) {
+
+				var latlng = new google.maps.LatLng(lat, lng);
+
+				$scope.geocoder.geocode({latLng: latlng}, function(results, status) {
+
+					if (status == google.maps.GeocoderStatus.OK) {
+
+						if (results[1]) {
+
+							var arrAddress = results;
+
+							$.each(arrAddress, function(i, address_component) {
+
+								if (address_component.types[0] == "locality") {
+
+									$scope.itemLocality = address_component.address_components[0].long_name;
+									$scope.$apply();
+
+								}
+
+							});
+
+						} else {
+
+							console.log("No results found");
+
+						}
+
+					} else {
+
+						console.log("Geocoder failed due to: " + status);
+
+					}
+
+				});
+
 			};
 			
 			// Get the latitude and the longitude;
-			var successFunction = function (position) {
-			  var lat = position.coords.latitude;
-			  var lng = position.coords.longitude;
-			  codeLatLng(lat, lng);
+			var successFunction = function(position) {
+
+				var lat = position.coords.latitude;
+				var lng = position.coords.longitude;
+
+				codeLatLng(lat, lng);
+
 			};
 
-			var errorFunction =  function () {
-			  alert("Geocoder failed");
+			var errorFunction = function() {
+
+				alert("Geocoder failed");
+
 			};
 			
-			if (navigator.geolocation) {
-			  navigator.geolocation.getCurrentPosition(successFunction, errorFunction);
-			}
+			if (navigator.geolocation)
+				navigator.geolocation.getCurrentPosition(successFunction, errorFunction);
+
 		};
+
 	});
 
 })();
