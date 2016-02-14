15a1d76bbeea85140aec32303c9e0e200bc71761
Renan Rodrigues <renan.rodrigues@mitahtech.com>
2016-02-14 21:30:01 -0200

    filtros da tela inicial

--- frontend/app/scripts/controllers/inicialController.js
+++ frontend/app/scripts/controllers/inicialController.js
@@ -4,8 +4,14 @@
 
 	modulo.controller('InicialController', function($scope, $rootScope, $location) {
 		//console.log('teste de console - InicialController');
-		$scope.procutFilter = {
-			name:null
+		$scope.produtcFilter = {
+			name: null,
+			categoria: null,
+			preco: null,
+			novo: true,
+			usado: true,
+			venda: true,
+			troca: true
 		};
 
 		$scope.productList = [{
