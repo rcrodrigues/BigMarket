6dae70f4aa9a84ee4bfba9d2a06a871bebebfb91
Julio Cezar <julio@casa>
2016-03-16 08:28:48 -0300

    Sistema de msg e cadastro de produto

--- backend/app/controllers/Produtos.java
+++ backend/app/controllers/Produtos.java
@@ -5,6 +5,13 @@ import play.mvc.Controller;
 
 public class Produtos extends Controller {
 
+	public static void create(Produto produto) {
+		
+		produto.save();
+		renderJSON("Produto cadastrado com sucesso!");
+		
+	}
+	
 	public static void getAll() {
 		
 		Produto produto = (Produto) Produto.findAll();
--- frontend/app/scripts/controllers/cadastrarEditarProdutoController.js
+++ frontend/app/scripts/controllers/cadastrarEditarProdutoController.js
@@ -2,7 +2,28 @@
 
 	var modulo = angular.module('mercadaoModule');
 
-	modulo.controller('CadastrarEditarProdutoController', function($scope, $rootScope, $location) {
+	modulo.controller('CadastrarEditarProdutoController', function($scope, $rootScope, $location, produtosService) {
+
+		$scope.produto = [];
+
+		$scope.createProduto = function() {
+
+			produtosService.createProduto($scope.produto,
+
+				function(sucesso) {
+
+					$scope.$emit('showMessageEvent', sucesso, 'success');
+
+				},
+				function(errorMessage) {
+
+					$scope.$emit('showMessageEvent', errorMessage, 'danger');
+
+				}
+
+			);
+
+		};
 
 
 	});
